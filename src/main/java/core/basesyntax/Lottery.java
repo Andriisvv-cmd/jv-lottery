fd14b0ede6b820b9c7d4225ba800f7cf05b8563b
Andrii <andriisopovskyi@gmail.com>
2026-05-25 10:59:32 +0200

    fix 2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,12 +2,15 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
+public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int MAX_BALL_NUMBER = 100;
+    private final Random random = new Random();
+
+
     public Ball getRandomBall() {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100) + 1;
-        Color randomColor = getRandomColor();
-        Ball ball = new Ball(randomColor, randomNumber);
-        return ball;
+        int randomNumber = random.nextInt(MAX_BALL_NUMBER) + 1;
+        Color randomColor = colorSupplier.getRandomColor();
+        return new Ball(randomColor, randomNumber);
     }
 }
